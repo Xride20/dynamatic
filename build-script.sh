43179b3ed720f45fafdda84b98fa336076c6434a
Alex <atp16@ic.ac.uk>
2021-05-02 13:50:30 +0000

    added build script

---
+++ build-script.sh
@@ -0,0 +1,30 @@
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo usermod -aG docker ${USER}
+
+su ${USER}
+echo "vagrant"
+
+sudo apt-get install -y make
+
+git clone https://github.com/Xride20/dynamatic.git
+cd dynamatic
+git submodule init
+git submodule update
+
+time make build-docker
